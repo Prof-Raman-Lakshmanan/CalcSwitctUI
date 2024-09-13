44a6f5104722770ae6900f98c0c9d63678c18811
Raman Lakshmanan <raman.l@monmouth.edu>
2024-09-13 13:31:34 -0400

    Initial Commit

---
+++ CalcSwitctUI/CalcSwitctUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcSwitctUIApp.swift
+//  CalcSwitctUI
+//
+//  Created by RamanLakshmanan on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CalcSwitctUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
